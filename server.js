9298a35711fe771c50f5298ffcbf380181cd60d2
sultanj21 <sabuhijleh@ggc.edu>
2025-11-17 23:48:03 -0500

    Fix package.json and switch to bcryptjs

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const multer = require('multer');
 const cors = require('cors');
